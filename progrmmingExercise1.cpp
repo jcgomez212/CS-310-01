28137247bdf9b2a2087f3fa43c1bb3706007fb01
jcgomez212 <91210626+jcgomez212@users.noreply.github.com>
2024-01-25 22:10:33 -0500

    Exercise 1

---
+++ progrmmingExercise1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 4, y = 10, a = 2, b = 2, c = 2;
+    cout << (3 + 4 * x) / 5 - 10 * (y - 5) * (a + b + c) / x + 9 * (4 / x + (9 + x) / y) << endl;
+
+
+    return 0;
+}
